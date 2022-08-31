98b9c6d3dd916f8cbece3949230378b1a1949596
yuval gofer <yuvyael@gmail.com>
2022-08-31 20:07:32 +0300

    41. issuing JWTs

--- DatingApp/API/Services/TokenService.cs
+++ DatingApp/API/Services/TokenService.cs
@@ -1,17 +1,42 @@
+using System.Security.Claims;
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config['TokenKey']));
+        }
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>{
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+            //after creating our token, we need token handler to create it
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //create token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            //return token
+            return tokenHandler.WriteToken(token);
         }
 
     }
