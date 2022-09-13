45a4e784d0fc72a1c31238187e415b6f3ce04211
yuval gofer <92877436+YuvalGofer@users.noreply.github.com>
2022-09-13 21:42:19 +0300

    64. Creating some more components

--- DatingApp/client/src/app/app-routing.module.ts
+++ DatingApp/client/src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'members',//localhost:4200/members
+    component: MembersListComponent
+  },
+  {
+    path: 'members/:id',//localhost:4200/members/1
+    component: MembersDetailComponent,
+  },
+  {
+    path: 'lists',
+    component: ListsComponent,
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent,
+  },
+  {
+    path:'**',
+    component: HomeComponent,
+    pathMatch:'full'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- DatingApp/client/src/app/app.module.ts
+++ DatingApp/client/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
 
 
 @NgModule({
@@ -17,7 +21,11 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MembersListComponent,
+    MembersDetailComponent,
+    MessagesComponent, 
+    ListsComponent
   ],
   imports: [
     BrowserModule,
