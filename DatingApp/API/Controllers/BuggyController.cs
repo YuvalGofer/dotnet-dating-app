589f85556720327a74e72e2dc650bc4ebf0cf732
yuval gofer <yuvyael@gmail.com>
2022-09-18 19:14:14 +0300

    76. Handling server error

--- DatingApp/API/Controllers/BuggyController.cs
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -21,27 +21,31 @@ namespace API.Controllers
         //create some methods to return different type of errors
         [Authorize]
         [HttpGet("auth")]
-        public ActionResult<string> GetSecret(){
+        public ActionResult<string> GetSecret()
+        {
             return "secret text";
         }
         //return not found
-        public ActionResult<AppUser> GetNotFound(){
+        public ActionResult<AppUser> GetNotFound()
+        {
             var thing = _context.Users.Find(-1);
-            if(thing == null) return NotFound();
-            return Ok(thing);  
+            if (thing == null) return NotFound();
+            return Ok(thing);
         }
         //return server error
         [HttpGet("Server-error")]
-        public ActionResult<string> GetServerError(){
+        public ActionResult<string> GetServerError()
+        {
             var thing = _context.Users.Find(-1);
             var thingToReturn = thing.ToString();
             return thingToReturn;
         }
         //return bad request
         [HttpGet("bad-request")]
-        public ActionResult<string>GetBadRequest(){
+        public ActionResult<string> GetBadRequest()
+        {
             return BadRequest("This was not a good request");
         }
-        
+
     }
 }
