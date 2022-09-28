782fe8b7dce16acf0c6137c379c592ee7ede898f
yuval gofer <92877436+YuvalGofer@users.noreply.github.com>
2022-09-28 17:46:44 +0300

    82. Adding a server error page

--- DatingApp/client/src/app/errors/test-errors/test-errors.component.ts
+++ DatingApp/client/src/app/errors/test-errors/test-errors.component.ts
@@ -7,47 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-errors.component.css']
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl = "https://localhost:5001/api/";
+  baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
+
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
   get404Error() {
-    this.http.get(this.baseUrl + 'buggy/not found').subscribe(response => {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error)
+      console.log(error);
     });
   }
   get400Error() {
-    this.http.get(this.baseUrl + 'buggy/bad request').subscribe(response => {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error)
+      console.log(error);
     });
   }
   get500Error() {
-    this.http.get(this.baseUrl + 'buggy/server error').subscribe(response => {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error)
+      console.log(error);
     });
   }
   get401Error() {
     this.http.get(this.baseUrl + 'buggy/auth').subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error)
+      console.log(error);
     });
   }
   get400ValidationError() {
-    this.http.get(this.baseUrl + 'buggy/register').subscribe(response => {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error)
+      console.log(error);
+      this.validationErrors = error;
     });
   }
-
 }
