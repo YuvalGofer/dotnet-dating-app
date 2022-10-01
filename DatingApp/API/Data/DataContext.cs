c39a00b11e1a7688acce0b1884691fe5e6f08074
yuval gofer <92877436+YuvalGofer@users.noreply.github.com>
2022-10-01 21:50:54 +0300

    87. Entity framework relationship

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -8,12 +8,6 @@ namespace API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
         public DbSet<AppUser> Users { get; set; }
-
-        internal Task SaveChangesAsync()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
