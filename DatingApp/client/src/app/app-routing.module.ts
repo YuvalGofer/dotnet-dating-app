9956a997cecf2b538133f1ef931fb8d9b1cb704d
yuval gofer <yuvyael@gmail.com>
2022-10-23 18:39:20 +0300

    115. Creating a member edit component

--- DatingApp/client/src/app/app-routing.module.ts
+++ DatingApp/client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 const routes: Routes = [
   {
@@ -24,6 +25,7 @@ const routes: Routes = [
         path: 'members', // localhost:4200/members
         loadChildren: () => import('./modules/members.module').then(mod => mod.MembersModule)
       },
+      {path: 'member/edit', component:MemberEditComponent},
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent }
     ]
