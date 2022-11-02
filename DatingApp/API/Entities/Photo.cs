048cc603d32f2d67cb51b6d96d7d5ee54866f020
yuval gofer <yuvyael@gmail.com>
2022-11-02 18:52:43 +0200

    135. Adding the main photo image to the nav bar

--- DatingApp/API/Entities/Photo.cs
+++ DatingApp/API/Entities/Photo.cs
@@ -7,11 +7,14 @@ namespace API.Entities
 {
     public class Photo
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+        //  this is 'fully defining' a relationship
         public AppUser AppUser { get; set; }
         public int AppUserId { get; set; }
     }
 }
+
+
